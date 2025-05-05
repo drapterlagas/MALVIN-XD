dcd547edea6996205c58968ac92baa5cbb244466
ⓃⒺCⓉOR🍯 <167437180+drapterlagas@users.noreply.github.com>
2025-05-05 23:07:14 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "malvin~k5QjWZab#BSfJMqJ1MTcAVcHhhabQ0lV776K-kbtJIU772drIvM4",
+SESSION_ID: process.env.SESSION_ID || "malvin~x9xSSbBA#G2rPovKtx_s64Dzm5jY6QFfhjAdaAQPh1TM5CF3PGsk",
 // add your Session Id make sure it starts with malvin~
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
